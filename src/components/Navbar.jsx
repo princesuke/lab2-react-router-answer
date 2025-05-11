98c014630063d9f85fe25847b74e118bf78f8754
prince <princesuke@gmail.com>
2025-05-11 15:21:30 +0700

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav style={{ padding: '1rem', background: '#eee' }}>
+      {/*  This is the navigation bar */}
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/pages/Subscribe.jsx
@@ -0,0 +1,10 @@
+function Subscribe() {
+  return (
+    <div>
+      <h1>Subscribe Page</h1>
+      <p>(Lab: สร้างฟอร์มสมัครรับข่าวสาร และเปลี่ยนหน้าเมื่อส่งสำเร็จ)</p>
+    </div>
+  );
+}
+
+export default Subscribe;
